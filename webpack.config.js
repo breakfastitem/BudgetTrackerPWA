c3107e352164ad03cef46a25b74de2bf689ba8c9
breakfastitem <andrew.ehrman@gmail.com>
2021-03-02 23:33:07 -0500

    Added web manifest to application

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
+const config = {
+    entry: "./public/index.js",
+    output: {
+      path: __dirname + "./dist",
+      filename: "bundle.js"
+    },
+    mode: "development",
+    plugins: [
+      new WebpackPwaManifest({
+        filename: "manifest.json",
+
+        inject:false,
+
+        fingerprints: false,
+
+        name: "Budget Tracker",
+        short_name: "Budget Tracker",
+        theme_color: "#eee",
+        background_color: "#eee",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "public/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "public/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+
+        ]
+      })
+
+    ]
+  };
+  
+  module.exports = config;
