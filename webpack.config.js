5e4ab366007a6257a0e1078c3bb4648f13c21fa8
breakfastitem <andrew.ehrman@gmail.com>
2021-03-03 03:27:53 -0500

    Changed version of web pack to finally resolve issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,43 @@
 const WebpackPwaManifest = require("webpack-pwa-manifest");
-
+const path = require("path");
 const config = {
-    entry: "/public/index.js",
-    output: {
-      path: __dirname + "/public/dist",
-      filename: "bundle.js"
-    },
-    mode: "development",
-    plugins: [
-      new WebpackPwaManifest({
-        filename: "manifest.json",
+  entry: "/public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "development",
+  plugins: [
+    new WebpackPwaManifest({
+      filename: "manifest.json",
 
-        inject:false,
+      inject: false,
 
-        fingerprints: false,
+      fingerprints: false,
 
-        name: "Budget Tracker",
-        short_name: "Budget Tracker",
-        theme_color: "#eee",
-        background_color: "#eee",
-        start_url: "/",
-        display: "standalone",
-        icons: [
-          {
-            src: "public/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "public/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
+      theme_color: "#eee",
+      background_color: "#eee",
+      start_url: "/",
+      display: "standalone",
+      icons: [
+        {
+          src: "public/icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
 
-        ]
-      })
+        },
+        {
+          src: "public/icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        }
 
-    ]
-  };
-  
-  module.exports = config;
+      ]
+    })
+
+  ]
+};
+
+module.exports = config;
