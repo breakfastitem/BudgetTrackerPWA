6ac5b454b77b5543552ea291e916b1a53876a9be
breakfastitem <andrew.ehrman@gmail.com>
2021-03-03 01:26:01 -0500

    Added boilerplate cacheing in service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE= [
+const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/index.js",
@@ -11,14 +11,53 @@ const FILES_TO_CACHE= [
     "/dist/manifest.json"
 ];
 
-const STATIC_CACHE= "static-cache-v1";
-const RUNTIME_CACHE= "runtime-cache";
+const STATIC_CACHE = "static-cache";
+const RUNTIME_CACHE = "runtime-cache";
 
-self.addEventListener("install", event =>{
+self.addEventListener("install", event => {
     event.waitUntil(
         caches
-        .open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(()=> self.skipWaiting())
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
+});
+
+self.addEventListener('activate', (event) => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME_CACHE).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
 });
